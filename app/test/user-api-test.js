6cb1468ff3858d5c6b1966a05667b2b179f7c263
Aidan <aidanob1713@hotmail.com>
2020-05-23 23:17:17 +0100

    Version2 - user api - islands api
    - user tests (working)
    - island tests (working - issue with addIsland it appears)

--- app/test/user-api-test.js
+++ app/test/user-api-test.js
@@ -1,15 +1,76 @@
 'use strict';
 
 const assert = require('chai').assert;
-const axios = require('axios');
-
+const FunnelService = require('./funnel-service');
+const fixtures = require('./fixtures.json');
+const _ = require('lodash');
 
 suite('User API tests', function () {
 
-    test('get users', async function () {
-        const response = await axios.get('http://localhost:3000/api/users');
-        const users = response.data;
-        assert.equal(3, users.length);
+    let users = fixtures.users;
+    let newUser = fixtures.newUser;
 
+    const funnelService = new FunnelService(fixtures.funnelService);
+
+    setup(async function () {
+        await funnelService.deleteAllUsers();
     });
+
+    teardown(async function () {
+        await funnelService.deleteAllUsers();
+    });
+
+    test('create a user', async function () {
+        const returnedUser = await funnelService.createUser(newUser);
+        assert(_.some([returnedUser], newUser), 'returnedUser must be a superset of newUser');
+        assert.isDefined(returnedUser._id);
+    });
+
+     test('get user', async function () {
+        const u1 = await funnelService.createUser(newUser);
+        const u2 = await funnelService.getUser(u1._id);
+        assert.deepEqual(u1, u2);
+    });
+
+    test('get invalid user', async function () {
+        const u1 = await funnelService.getUser('1234');
+        assert.isNull(u1);
+        const u2 = await funnelService.getUser('012345678901234567890123');
+        assert.isNull(u2);
+    });
+
+
+    test('delete a user', async function () {
+        let u = await funnelService.createUser(newUser);
+        assert(u._id != null);
+        await funnelService.deleteOneUser(u._id);
+        u = await funnelService.getUser(u._id);
+        assert(u == null);
+    });
+
+    test('get all users', async function () {
+        for (let u of users) {
+            await funnelService.createUser(u);
+        }
+
+        const allUsers = await funnelService.getUsers();
+        assert.equal(allUsers.length, users.length);
+    });
+
+    test('get users detail', async function () {
+        for (let u of users) {
+            await funnelService.createUser(u);
+        }
+
+        const allUsers = await funnelService.getUsers();
+        for (var i = 0; i < users.length; i++) {
+            assert(_.some([allUsers[i]], users[i]), 'returnedUser must be a superset of newUser');
+        }
+    });
+
+    test('get all users empty', async function () {
+        const allUsers = await funnelService.getUsers();
+        assert.equal(allUsers.length, 0);
+    });
+
 });
--- routes-api.js
+++ routes-api.js
@@ -1,10 +1,17 @@
 const Users= require('./app/api/users');
+const Islands= require('./app/api/islands');
+
 module.exports = [
 
 { method: 'GET', path: '/api/users', config: Users.find },
 { method: 'GET', path: '/api/users/{id}', config: Users.findOne },
 { method: 'POST', path: '/api/users', config: Users.create },
 { method: 'DELETE', path: '/api/users/{id}', config: Users.deleteOne },
-{ method: 'DELETE', path: '/api/users', config: Users.deleteAll }
+{ method: 'DELETE', path: '/api/users', config: Users.deleteAll },
+
+{ method: 'GET', path: '/api/islands', config: Islands.findAll },
+{ method: 'GET', path: '/api/users/{id}/islands', config: Islands.findByUser },
+{ method: 'POST', path: '/api/user/{id}/islands', config: Islands.addIsland }
+
 
 ];
